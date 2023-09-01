109c2a92e3c6968d2083114803977d707200108c
ranjan <rockytomy20@gmail.com>
2023-09-01 14:21:00 +0530

    subtraction function added to the file

--- python_prog/arith.py
+++ python_prog/arith.py
@@ -4,3 +4,10 @@ def add(a,b):
 n=int(input("enter a number="))
 m=int(input("enter a number="))
 print(add(n,m))
+
+def sub(a,b):
+ return(a-b)
+
+n=int(input("enter a number="))
+m=int(input("enter a number="))
+print(sub(n,m))
